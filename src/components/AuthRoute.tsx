f88cfc6842387fc9847dded6e9b595e14cda34c2
Casper Deseyne <casper.deseyne@gmail.com>
2019-03-07 16:13:19 +0100

    Public/Private Route Component

---
+++ src/components/AuthRoute.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { RootState } from '../store/types'
+
+const mapStateToProps = (state: RootState) => ({
+  isAuthenticated: state.auth.isAuthenticated
+})
+
+type Props = React.ComponentProps<typeof Route> & {
+  component: React.ComponentType,
+  isAuthenticated: boolean,
+  redirectTo: string,
+  privateRoute?: boolean
+} & ReturnType<typeof mapStateToProps>
+
+const AuthRoute: React.FC<Props> = ({
+  redirectTo,
+  component: Component,
+  isAuthenticated,
+  render,
+  privateRoute = false,
+  ...routeProps
+}) => {
+  return (
+    <Route {...routeProps} render={(props) => {
+      return privateRoute === isAuthenticated
+        ? <Component {...props} />
+        : <Redirect to={redirectTo} />
+    }} />
+  )
+}
+
+export default connect(mapStateToProps)(AuthRoute)
