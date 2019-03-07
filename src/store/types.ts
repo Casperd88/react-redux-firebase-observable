01b8122c920532fe0f6f4be45c63a0f82efbd3df
Casper Deseyne <casper.deseyne@gmail.com>
2019-03-07 16:18:43 +0100

    cleanup unused types

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,12 +1,4 @@
 import { StateType } from 'typesafe-actions'
 import { rootReducer } from './'
 
-export interface Action<T> {
-  type: T
-}
-
-export interface PayloadAction<T, P> extends Action<T> {
-  payload: P
-}
-
 export type RootState = StateType<typeof rootReducer>
